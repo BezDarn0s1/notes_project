fb40d00bec78a8d27cd19af197a8d449936381f4
Garfeld <misina219@gmail.com>
2022-02-16 18:56:58 +0300

    3

--- main/note_board/urls.py
+++ main/note_board/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home_page)
+    path('', views.home_page, name='home'),
+    path('/notes', views.notes_page, name='notes')
 
 ]
--- main/note_board/views.py
+++ main/note_board/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home_page(request):
     return render (request, 'note_board/home.html')
+
+
+def notes_page(request):
+    return render (request, 'note_board/notes.html')
